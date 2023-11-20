816711f68a32dcda438a390c6be18d3618023cc8
Yan Almeida <yan.almeida91@hotmail.com>
2023-11-20 03:55:14 -0300

    feat: inclusao de leitura da linha de comando para decisao do algoritmo

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import sys
+
 from src.compressors.huffman import Huffman
 from src.compressors.lz77 import LZ77
 from src.compressors.lz78 import LZ78
 from src.streaming import execute_streaming
 
+ALGORITHMS = {
+    "huffman": (Huffman,),
+    "lz77": (LZ77, True),
+    "lz78": (LZ78, True)
+}
+
 if __name__ == '__main__':
-    execute_streaming(Huffman)
-    execute_streaming(LZ77, serialization=True)
-    execute_streaming(LZ78, serialization=True)
+    execute_streaming(*ALGORITHMS[sys.argv[1].lower()])
